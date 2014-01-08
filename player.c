a569cecd7107917e66c33f366a6f49eeaf27949b
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2014-01-08 08:11:57 -0500

    fix player

--- player.c
+++ player.c
@@ -4,6 +4,21 @@
 
 #include "GStreamerUtilities.h"
 
+// GstPlayFlags flags from playbin2. It is the policy of GStreamer to
+// not publicly expose element-specific enums. That's why this
+// GstPlayFlags enum has been copied here.
+typedef enum {
+    GST_PLAY_FLAG_VIDEO         = 0x00000001,
+    GST_PLAY_FLAG_AUDIO         = 0x00000002,
+    GST_PLAY_FLAG_TEXT          = 0x00000004,
+    GST_PLAY_FLAG_VIS           = 0x00000008,
+    GST_PLAY_FLAG_SOFT_VOLUME   = 0x00000010,
+    GST_PLAY_FLAG_NATIVE_AUDIO  = 0x00000020,
+    GST_PLAY_FLAG_NATIVE_VIDEO  = 0x00000040,
+    GST_PLAY_FLAG_DOWNLOAD      = 0x00000080,
+    GST_PLAY_FLAG_BUFFERING     = 0x000000100
+} GstPlayFlags;
+
 typedef struct {
     GstElement* playBin;
     GstElement* fpsSink;
@@ -72,7 +87,7 @@ static GstElement* createVideoSink(MediaPlayerPrivateGStreamer *m)
 
     m->fpsSink = gst_element_factory_make("fpsdisplaysink", "sink");
     if (m->fpsSink) {
-        g_object_set(m->fpsSink, "silent", TRUE, "text-overlay", FALSE, "video-sink", videoSink, NULL);
+        g_object_set(m->fpsSink, "silent", TRUE, "text-overlay", FALSE, "video-sink", m->webkitVideoSink, NULL);
         videoSink = m->fpsSink;
     }
 
@@ -98,9 +113,66 @@ static void createGSTPlayBin(MediaPlayerPrivateGStreamer *m)
     g_object_set(m->playBin, "video-sink", createVideoSink(m), NULL);
 }
 
+static bool changePipelineState(MediaPlayerPrivateGStreamer* m, GstState newState)
+{
+    assert(m->playBin);
+
+    GstState currentState;
+    GstState pending;
+
+    gst_element_get_state(m->playBin, &currentState, &pending, 0);
+    if (currentState == newState || pending == newState) {
+        return true;
+    }
+
+    GstStateChangeReturn setStateResult = gst_element_set_state(m->playBin, newState);
+    GstState pausedOrPlaying = newState == GST_STATE_PLAYING ? GST_STATE_PAUSED : GST_STATE_PLAYING;
+    if (currentState != pausedOrPlaying && setStateResult == GST_STATE_CHANGE_FAILURE) {
+        return false;
+    }
+
+    return true;
+}
+
+static void setDownloadBuffering(MediaPlayerPrivateGStreamer* m)
+{
+   assert(m->playBin);
+
+    GstPlayFlags flags;
+    g_object_get(m->playBin, "flags", &flags, NULL);
+
+    if (flags & GST_PLAY_FLAG_DOWNLOAD)
+        return;
+
+    g_object_set(m->playBin, "flags", flags | GST_PLAY_FLAG_DOWNLOAD, NULL);
+}
+
+
+static void load(MediaPlayerPrivateGStreamer *m, const char* uri)
+{
+    assert(m->playBin);
+
+    g_free(m->url);
+    m->url = g_strdup(uri);
+    g_object_set(m->playBin, "uri", uri, NULL);
+
+    /* commitLoad */
+    changePipelineState(m, GST_STATE_PAUSED);
+    setDownloadBuffering(m);
+}
+
+static void play(MediaPlayerPrivateGStreamer *m)
+{
+    if (!changePipelineState(m, GST_STATE_PLAYING)) {
+        g_printerr("Play failed!\n");
+    }
+}
+
 static void destroy(MediaPlayerPrivateGStreamer *m)
 {
     assert(m);
+
+    g_free(m->url);
 
     if (m->repaintHandler != 0)
         g_signal_handler_disconnect(m->webkitVideoSink, m->repaintHandler);
@@ -109,6 +181,7 @@ static void destroy(MediaPlayerPrivateGStreamer *m)
         GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(GST_PIPELINE(m->playBin)));
         g_signal_handlers_disconnect_by_func(bus, mediaPlayerPrivateMessageCallback, m);
         gst_bus_remove_signal_watch(bus);
+        g_object_unref(bus);
 
         gst_element_set_state(m->playBin, GST_STATE_NULL);
 
@@ -119,6 +192,14 @@ static void destroy(MediaPlayerPrivateGStreamer *m)
         g_main_loop_unref(m->loop);
 }
 
+static gboolean
+launch(gpointer data)
+{
+    play((MediaPlayerPrivateGStreamer *) data);
+
+    return FALSE;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -126,15 +207,13 @@ main(int argc, char **argv)
         return -1;
 
     MediaPlayerPrivateGStreamer *m = g_new0(MediaPlayerPrivateGStreamer, 1);
+    createGSTPlayBin(m);
 
     int i = 1;
     while(--argc) {
-
-        m->url = argv[i++];
-
-        createGSTPlayBin(m);
-
+        load(m, argv[i--]);
         m->loop = g_main_loop_new(NULL, TRUE);
+        g_idle_add(launch, m);
         g_main_loop_run(m->loop);
     }
 
